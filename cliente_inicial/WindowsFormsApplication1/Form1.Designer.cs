ca5fa08c202374157742513c96aeb567922d6d22
JordiLlaveria <jordi.llaveria@estudiantat.upc.edu>
2020-10-21 17:29:14 +0200

    Tercera persion

--- cliente_inicial/WindowsFormsApplication1/Form1.Designer.cs
+++ cliente_inicial/WindowsFormsApplication1/Form1.Designer.cs
@@ -46,6 +46,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
             this.label2.Location = new System.Drawing.Point(31, 31);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
